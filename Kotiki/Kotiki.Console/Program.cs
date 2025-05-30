5a9c450fa6ce3868723329e02c9c9d0cb36f2a35
MaybeMeow2 <129261839+MaybeMeow2@users.noreply.github.com>
2025-05-30 10:36:58 +0300

    Update Program.cs

--- Kotiki/Kotiki.Console/Program.cs
+++ Kotiki/Kotiki.Console/Program.cs
@@ -5,13 +5,13 @@ class Program
 {
     static void Main()
     {
-        // Створюємо нового котика
+       
         Cat myCat = new Cat("Мурчик", "Чорний", 3);
 
-        // Виводимо інформацію про кота
+       
         Console.WriteLine(myCat);
 
-        // Викликаємо метод, щоб кіт "мявкнув"
+     
         myCat.Meow();
 
         Console.WriteLine("\nНатисніть будь-яку клавішу для виходу...");
