21b86155e0e8e6ba2a17b73c088c769f34c37d29
MaybeMeow2 <129261839+MaybeMeow2@users.noreply.github.com>
2025-05-30 10:38:31 +0300

    Update Cat.cs

--- Kotiki/Kotiki.Common/Cat.cs
+++ Kotiki/Kotiki.Common/Cat.cs
@@ -9,7 +9,7 @@ namespace Kotiki.Common
         public string Color { get; set; }
         public int Age { get; set; }
 
-        // Конструктор
+  
         public Cat(string name, string color, int age)
         {
             Id = Guid.NewGuid();
@@ -18,13 +18,13 @@ namespace Kotiki.Common
             Age = age;
         }
 
-        // Метод для виводу інформації
+    
         public void Meow()
         {
             Console.WriteLine($"{Name} каже: Мяу!");
         }
 
-        // Перевизначення ToString для зручного виводу
+     
         public override string ToString()
         {
             return $"Кіт {Name}, Колір: {Color}, Вік: {Age} років";
